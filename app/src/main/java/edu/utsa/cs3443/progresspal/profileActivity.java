607c19f126b78c5e5871b6ec4796bdf398e26b05
Mathew Robillard <bryanrobillard29@yahoo.com>
2024-10-31 00:03:28 -0500

    nav bar added to profile

--- app/src/main/java/edu/utsa/cs3443/progresspal/profileActivity.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/profileActivity.java
@@ -1,6 +1,9 @@
 package edu.utsa.cs3443.progresspal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,46 @@ public class profileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button profileButton = findViewById(R.id.profile_button);
+        Button pointButton = findViewById(R.id.points_button);
+        Button creditButton = findViewById(R.id.credits_button);
+        Button homeButton = findViewById(R.id.home_button);
+
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchHome();
+            }
+        });
+
+        pointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPoint();
+            }
+        });
+
+        creditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCredit();
+            }
+        });
+    }
+
+    private void launchPoint(){
+        Intent intentPoint = new Intent(this, pointsActivity.class);
+        startActivity(intentPoint);
+    }
+    private void launchCredit(){
+        Intent intentCredit = new Intent(this, creditsActivity.class);
+        startActivity(intentCredit);
+    }
+
+    private void launchHome(){
+        Intent intentHome = new Intent(this, homeActivity.class);
+        startActivity(intentHome);
     }
 }
