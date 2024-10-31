c080d69592f8e31dc26288dee00bfb920a9a2b6c
Mathew Robillard <bryanrobillard29@yahoo.com>
2024-10-31 00:48:32 -0500

    nav bar added to points

--- app/src/main/java/edu/utsa/cs3443/progresspal/pointsActivity.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/pointsActivity.java
@@ -1,12 +1,11 @@
 package edu.utsa.cs3443.progresspal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class pointsActivity extends AppCompatActivity {
 
@@ -14,5 +13,45 @@ public class pointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+
+        Button profileButton = findViewById(R.id.profile_button);
+        Button pointButton = findViewById(R.id.points_button);
+        Button creditButton = findViewById(R.id.credits_button);
+        Button homeButton = findViewById(R.id.home_button);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchHome();
+            }
+        });
+
+        pointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchProfile();
+            }
+        });
+
+        creditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCredit();
+            }
+        });
+    }
+
+    private void launchProfile(){
+        Intent intentProfile = new Intent(this, profileActivity.class);
+        startActivity(intentProfile);
+    }
+    private void launchCredit(){
+        Intent intentCredit = new Intent(this, creditsActivity.class);
+        startActivity(intentCredit);
+    }
+
+    private void launchHome(){
+        Intent intentHome = new Intent(this, homeActivity.class);
+        startActivity(intentHome);
     }
 }
