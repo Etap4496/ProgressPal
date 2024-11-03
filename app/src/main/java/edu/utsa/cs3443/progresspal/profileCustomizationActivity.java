11bdf6f28587b50dbc13040fd3ff7e627602767a
Mathew Robillard <bryanrobillard29@yahoo.com>
2024-11-03 03:17:47 -0600

    basic drop down menu added

--- app/src/main/java/edu/utsa/cs3443/progresspal/profileCustomizationActivity.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/profileCustomizationActivity.java
@@ -3,7 +3,10 @@ package edu.utsa.cs3443.progresspal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class profileCustomizationActivity extends AppCompatActivity {
+public class profileCustomizationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,16 @@ public class profileCustomizationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_customization);
 
         Button backButton = findViewById(R.id.back_button);
+        Spinner spinnerColors = findViewById(R.id.mascot_colors);
+
+        spinnerColors.setOnItemSelectedListener(this);
+
+        String[] colors = getResources().getStringArray(R.array.mascot_colors);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, colors);
+        adapter.setDropDownViewResource(androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item);
+        spinnerColors.setAdapter(adapter);
+
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +44,15 @@ public class profileCustomizationActivity extends AppCompatActivity {
     private void launchBack(){
         Intent intentBack = new Intent(this, profileActivity.class);
         startActivity(intentBack);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        //implement method for color change of mascot
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
