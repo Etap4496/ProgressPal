ad919ba6b1d7c66f34cec07211aa1ced3184cb8e
eli <eli.tapia@my.utsa.edu>
2024-11-04 16:28:30 -0600

    color class edited

--- app/src/main/java/edu/utsa/cs3443/progresspal/model/Inventory.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/model/Inventory.java
@@ -1,12 +1,19 @@
 package edu.utsa.cs3443.progresspal.model;
 
+import android.app.Activity;
+import android.content.res.AssetManager;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Inventory {
     ArrayList<Hat> hats;
     ArrayList<Staff> staffs;
     ArrayList<Color> colors;
-    private int totalxp;
+    int totalxp;
 
     public Inventory() {
         hats = new ArrayList<Hat>();
@@ -31,7 +38,8 @@ public class Inventory {
         this.staffs = staffs;
     }
 
-    public ArrayList<Color> getColors() {
+    public ArrayList<Color> getColors(ArrayList<Color> colors) {
+
         return colors;
     }
 
@@ -39,7 +47,47 @@ public class Inventory {
         this.colors = colors;
     }
 
-    public int getTotalxp(){
+
+    public void loadColorsFromCSV(Activity activity) {
+        AssetManager manager = activity.getAssets();
+        Scanner scan = null;
+        String color = "colorsID.csv";
+
+        try {
+            InputStream is = manager.open(color);
+            String line = null;
+            scan = new Scanner(is);
+            scan.nextLine();
+
+
+            while (scan.hasNextLine()) {
+
+                String Line = scan.nextLine();
+                String[] tokens = line.split(",");
+                int ID = Integer.parseInt(tokens[0]);
+
+                addColor(new Color(ID, tokens[1], tokens[2], false));
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Color> getColors() {
+        return colors;
+    }
+
+
+    public int getTotalxp() {
         return totalxp;
     }
+
+    public void addColor(Color color) {
+        colors.add(color);
+    }
+
 }
+
