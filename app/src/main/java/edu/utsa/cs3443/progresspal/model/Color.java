26f0ed61af0c35b2c04b171f16c8efaf1867217a
eli <eli.tapia@my.utsa.edu>
2024-11-04 16:23:50 -0600

    color class edited

--- app/src/main/java/edu/utsa/cs3443/progresspal/model/Color.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/model/Color.java
@@ -1,5 +1,16 @@
 package edu.utsa.cs3443.progresspal.model;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Color {
     private int ID;
     private boolean equipped;
@@ -13,35 +24,47 @@ public class Color {
         this.equipped = equipped;
     }
 
-    private void setFilename(String filename){
-        this.filename = filename;
-    }
-
-    private String getFilename(){
-        return filename;
-    }
-
-    private void setName(){
-        this.name = name;
-    }
-
-    private String getName(){
-        return name;
-    }
-
+    // Getter and Setter for ID
     public int getID() {
         return ID;
     }
-
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    // Getter and Setter for equipped
     public boolean isEquipped() {
         return equipped;
     }
-
     public void setEquipped(boolean equipped) {
         this.equipped = equipped;
     }
+
+    // Getter and Setter for filename
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename.toLowerCase();
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Color{" +
+                "ID=" + ID +
+                ", equipped=" + equipped +
+                ", filename='" + filename + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
