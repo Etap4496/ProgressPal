d6bd36997947773e0ef877db2cef894771391011
marcdjbn <marcdjabian@gmail.com>
2024-11-05 14:40:08 -0600

    homeact dynamic setup

--- app/src/main/java/edu/utsa/cs3443/progresspal/model/TaskTracker.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/model/TaskTracker.java
@@ -1,8 +1,12 @@
 package edu.utsa.cs3443.progresspal.model;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskTracker {
 
@@ -12,18 +16,39 @@ public class TaskTracker {
         tasks = new ArrayList<Task>();
     }
 
-    private void loadTasks(Activity activity){
+    public void loadTasks(Activity activity){
+        AssetManager manager = activity.getAssets();
+
+        try{
+            InputStream file = manager.open("tasks.csv");
+            Scanner scan = new Scanner(file);
+
+            scan.nextLine();
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                String[] tokens = line.split(",");
+                String name = tokens[0];
+                String dueDate = tokens[1];
+                int time = Integer.parseInt(tokens[2]);
+                boolean status = Boolean.parseBoolean(tokens[3]);
+                int xp = time * 50;
+
+                addTasks(new Task(name, dueDate, time, status, xp));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void addTasks(Task task){
+    public void addTasks(Task task){
         tasks.add(task);
     }
 
-    private void createTask(){
+    public void createTask(){
 
     }
 
-    private void deleteTask(){
+    public void deleteTask(){
 
     }
 
