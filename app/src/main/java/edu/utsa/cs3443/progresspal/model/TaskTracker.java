edf7ea1c13aa5b68067fbd9669294a9a9d2448ab
marcdjbn <marcdjabian@gmail.com>
2024-11-05 19:13:18 -0600

    fix task tracker

--- app/src/main/java/edu/utsa/cs3443/progresspal/model/TaskTracker.java
+++ app/src/main/java/edu/utsa/cs3443/progresspal/model/TaskTracker.java
@@ -1,8 +1,12 @@
 package edu.utsa.cs3443.progresspal.model;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskTracker {
 
@@ -12,21 +16,33 @@ public class TaskTracker {
         tasks = new ArrayList<Task>();
     }
 
-    private void loadTasks(Activity activity){
+    public void loadTasks(Activity activity){
+        AssetManager manager = activity.getAssets();
 
+        try{
+            InputStream file = manager.open("tasks.csv");
+            Scanner scan = new Scanner(file);
+
+            scan.nextLine();
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                String[] tokens = line.split(",");
+                String name = tokens[0];
+                String dueDate = tokens[1];
+                int time = Integer.parseInt(tokens[2]);
+
+                addTasks(new Task(name, dueDate, time));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void addTasks(Task task){
+    public void addTasks(Task task){
         tasks.add(task);
     }
 
-    private void createTask(){
-
+    public void deleteTask(Task task){
+        tasks.remove(task);
     }
-
-    private void deleteTask(){
-
-    }
-
-
 }
